5a7717f687b78351ac06c642ebd89db2baac78b1
Hasani Gordon <hasanigordon@gmail.com>
2026-03-09 09:39:32 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "./pages/utils";
-import { Home, CalendarCheck, MessageCircle, TrendingUp, MapPin } from "lucide-react";
+import { Home, CalendarCheck, MessageCircle, TrendingUp, Settings } from "lucide-react";
 
 const NAV_ITEMS = [
   { name: "Home",      icon: Home,          page: "Home" },
   { name: "Check In",  icon: CalendarCheck, page: "DailyCheckIn" },
   { name: "Progress",  icon: TrendingUp,    page: "ClientProgress" },
   { name: "Messages",  icon: MessageCircle, page: "ParticipantMessages" },
-  { name: "Find Help", icon: MapPin,        page: "FindHelpNow" },
+  { name: "Settings",  icon: Settings,      page: "ClientSettings" },
 ];
 
 
